c04b53c13190640e1059e37258850b5c2dd301b6
datadogmd <mdusaire50@gmail.com>
2025-12-30 23:19:08 -0600

    used the default dependency implementations generated by Android Studio rather than the specific versions, as suggested by Copilot.
    There were also some code changes to the ExampleUnitTest and ExampleInstrumentedTest following the AI suggestions, substituting compilerOptions for the deprecated kotlinOptions, in build.gradle.kts (:app)
    
    Built an APK and installed on my A13.
    
    Commit and Push don't like that the API level is lower than 33.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,6 @@
     alias(libs.plugins.kotlin.android) apply false
 }*/
 plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.android.application") version "8.13.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.0" apply false
 }
